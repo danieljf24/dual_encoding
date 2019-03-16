b5b5842e3045a7776a07f5623b6b2e003aa51415
danieljf24 <danieljf24@163.com>
2019-03-16 16:46:05 +0800

    update url

--- do_get_dataset.sh
+++ do_get_dataset.sh
@@ -2,8 +2,8 @@ ROOTPATH=$HOME/VisualSearch
 mkdir -p $ROOTPATH && cd $ROOTPATH
 
 # download and extract dataset
-wget http://lixirong.net/data/dual_encoding/msrvtt10k.tar.gz
-tar zxf msrvtt10k.tar.gz
+wget http://lixirong.net/data/cvpr2019/msrvtt10k-text-and-resnet-152-img1k.tar.gz
+tar zxf msrvtt10k-text-and-resnet-152-img1k.tar.gz
 
 # download and extract pre-trained word2vec
 wget http://lixirong.net/data/w2vv-tmm2018/word2vec.tar.gz
