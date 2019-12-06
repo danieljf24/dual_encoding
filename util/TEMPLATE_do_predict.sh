6a6ace55c14508db36dc2d971aa7f550e27f6cf3
xuchaoxi <xuchaoxikb@163.com>
2019-12-06 14:38:45 +0800

    fix do_predict template

--- util/TEMPLATE_do_predict.sh
+++ util/TEMPLATE_do_predict.sh
@@ -6,4 +6,4 @@ query_sets=@@@query_sets@@@
 
 gpu=0
 
-CUDA_VISIBLE_DEVICES=$gpu python tester.py $testCollection --rootpath $rootpath --overwrite $overwrite --logger_name $logger_name --query_sets $query_sets
+CUDA_VISIBLE_DEVICES=$gpu python predictor.py $testCollection --rootpath $rootpath --overwrite $overwrite --logger_name $logger_name --query_sets $query_sets
